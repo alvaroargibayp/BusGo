0544227b1191da0bffcd4b428af99dd4018de312
oscarcastillofdez <oscarcastillofdez@gmail.com>
2024-03-25 18:10:01 +0100

    Haciendo llamadas al servidor

--- app/src/main/java/udc/psi/busgo/MainActivity.java
+++ app/src/main/java/udc/psi/busgo/MainActivity.java
@@ -3,12 +3,56 @@ package udc.psi.busgo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import org.chromium.net.CronetEngine;
+import org.chromium.net.UrlRequest;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "_TAG";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.i(TAG, "comienzo");
+
+        String url = "https://www.example.com/"; // URL de la API que deseas consultar
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                Request.Method.GET,
+                "https://itranvias.com/queryitr_v3.php?func=7&dato=20160101T000000_gl_0_20160101T000000",
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d(TAG, response.toString());
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // Maneja errores aquí
+                    }
+                }
+        );
+
+        requestQueue.add(jsonObjectRequest);
     }
 }
+
