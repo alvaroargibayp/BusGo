2f1ae596c995ab619f5eee126a325d5cbb8222a2
Álvaro Argibay <91668923+alvaroargibayp@users.noreply.github.com>
2024-04-21 17:34:54 +0200

    feature/04_busqueda_individual

--- app/src/main/java/udc/psi/busgo/tabs/HomeTab.java
+++ app/src/main/java/udc/psi/busgo/tabs/HomeTab.java
@@ -4,63 +4,81 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 import udc.psi.busgo.R;
+import udc.psi.busgo.databinding.FragmentHomeTabBinding;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeTab#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class HomeTab extends Fragment {
+public class HomeTab extends Fragment implements View.OnClickListener {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public HomeTab() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeTab.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeTab newInstance(String param1, String param2) {
-        HomeTab fragment = new HomeTab();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private static final String TAG = "_TAG";
+    FragmentHomeTabBinding binding;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_tab, container, false);
+        binding = FragmentHomeTabBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
+        binding.btnSearch.setOnClickListener(this);
+
+        return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == binding.btnSearch) {
+            if (binding.etSearch.getText().toString().isEmpty()) {
+                Toast.makeText(binding.getRoot().getContext(), "Por favor, introduce un valor válido", Toast.LENGTH_SHORT).show();
+            } else {
+                individual_search();
+            }
+        } else {
+            Log.d(TAG, "Click desconocido");
+        }
+    }
+
+    public void individual_search() {
+        Log.d(TAG, "searchIndividual");
+        RequestQueue requestQueue = Volley.newRequestQueue(getActivity().getApplicationContext());
+
+        String baseUrl;
+        if (binding.switchToggle.isChecked()) {
+            baseUrl = "https://bus.delthia.com/api/linea/" + binding.etSearch.getText().toString();
+        } else {
+            baseUrl = "https://bus.delthia.com/api/parada/" + binding.etSearch.getText().toString();
+        }
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                Request.Method.GET,
+                baseUrl,
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d("_TAG", response.toString());
+                        binding.tvSearchContent.setText(response.toString());
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // Maneja errores aquí
+                    }
+                }
+        );
+
+        requestQueue.add(jsonObjectRequest);
     }
 }
