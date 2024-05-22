385b83292b366128e4327e8b1d89dab7249728bb
juan <juan.villaverde.rodriguez@udc.es>
2024-05-22 18:56:57 +0200

    añadido boton al widget para que pueda cambiar el textView incorporado, ya lo hace

--- app/src/main/java/udc/psi/busgo/widgets/StopsWidget.java
+++ app/src/main/java/udc/psi/busgo/widgets/StopsWidget.java
@@ -1,44 +1,60 @@
 package udc.psi.busgo.widgets;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
 import android.widget.RemoteViews;
+
+import java.util.Random;
 
 import udc.psi.busgo.R;
 
-/**
- * Implementation of App Widget functionality.
- */
 public class StopsWidget extends AppWidgetProvider {
 
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-                                int appWidgetId) {
+    private static final String ACTION_UPDATE_WIDGET = "ACTION_UPDATE_WIDGET";
+    private static final CharSequence[] refreshStrings = {"AAAAAAAAA", "BBBBBBBBBBB", "CCCCCCCCCCC"};
 
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
-        // Construct the RemoteViews object
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
+        int index = new Random().nextInt(refreshStrings.length);
+        CharSequence stringText = refreshStrings[index];
+        Log.d("_TAG23", "UPDATEEEEEEEEEEEEEEEE");
+        Log.d("_TAG23", (String) stringText);
+        Log.d("_TAG23", String.valueOf(index));
+
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.stops_widget);
-        views.setTextViewText(R.id.stopsWidgetUndefinedStopsId, widgetText);
+        views.setTextViewText(R.id.stopsWidgetUndefinedStopsId, stringText);
 
-        // Instruct the widget manager to update the widget
+        Intent intent = new Intent(context, StopsWidget.class);
+        intent.setAction(ACTION_UPDATE_WIDGET);
+        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
+        views.setOnClickPendingIntent(R.id.stopsWidgetUndefinedStopsButton, pendingIntent);
+
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        // There may be multiple widgets active, so update all of them
         for (int appWidgetId : appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId);
         }
     }
 
     @Override
-    public void onEnabled(Context context) {
-        // Enter relevant functionality for when the first widget is created
-    }
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
 
-    @Override
-    public void onDisabled(Context context) {
-        // Enter relevant functionality for when the last widget is disabled
+        //Actualiza el widget correspondiente
+        if (ACTION_UPDATE_WIDGET.equals(intent.getAction())) {
+            int appWidgetId = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
+            if (appWidgetId != AppWidgetManager.INVALID_APPWIDGET_ID) {
+                AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+                updateAppWidget(context, appWidgetManager, appWidgetId);
+            }
+        }
     }
 }
