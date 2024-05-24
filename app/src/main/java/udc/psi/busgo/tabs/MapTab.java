d339029e40d952206bff77789435caf1303b9368
juan <juan.villaverde.rodriguez@udc.es>
2024-05-24 19:33:22 +0200

    merge con rama de widgets

--- app/src/main/java/udc/psi/busgo/tabs/MapTab.java
+++ app/src/main/java/udc/psi/busgo/tabs/MapTab.java
@@ -1,5 +1,10 @@
 package udc.psi.busgo.tabs;
 
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,19 +12,30 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+import udc.psi.busgo.MainActivity;
 import udc.psi.busgo.R;
+import udc.psi.busgo.databinding.FragmentMapTabBinding;
+import udc.psi.busgo.databinding.FragmentStopsTabBinding;
+import udc.psi.busgo.widgets.StopsWidget;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MapTab#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class MapTab extends Fragment {
-    private static final String GOOGLE_MAPS_URL = "https://openlayers.org/en/latest/examples/mobile-full-screen.html";
+
+public class MapTab extends Fragment implements View.OnClickListener {
+
+    Button stop1Button;
+    Button stop2Button;
+    Button stop3Button;
+
+    FragmentMapTabBinding binding;
+
+    private static final String ACTION_UPDATE_WIDGET = "ACTION_UPDATE_WIDGET";
+
+
+
 
     WebView mapWebView;
     public MapTab() {
@@ -42,7 +58,58 @@ public class MapTab extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map_tab, container, false);
+        binding = FragmentMapTabBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
+
+        stop1Button = binding.buttonParada1;
+        stop1Button.setOnClickListener(this);
+
+        stop2Button = binding.buttonParada2;
+        stop2Button.setOnClickListener(this);
+
+        stop3Button = binding.buttonParada3;
+        stop3Button.setOnClickListener(this);
+
+
+        return view;
+    }
+
+    void createWidget(String stopName) {
+        AppWidgetManager mAppWidgetManager = requireActivity().getSystemService(AppWidgetManager.class);
+
+        ComponentName myProvider = new ComponentName(requireActivity().getApplicationContext(), StopsWidget.class);
+
+        Bundle b = new Bundle();
+        b.putString("ggg", "ggg");
+
+        //Comprueba si el lanzador sopporta la creación de widgets
+        if (mAppWidgetManager.isRequestPinAppWidgetSupported()) {
+            Intent pinnedWidgetCallbackIntent = new Intent(requireActivity().getApplicationContext(), StopsWidget.class);
+            //pinnedWidgetCallbackIntent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+            pinnedWidgetCallbackIntent.setAction(ACTION_UPDATE_WIDGET);
+
+            pinnedWidgetCallbackIntent.putExtra("KEY_STOP_NAME", stopName);
+            PendingIntent successCallback = PendingIntent.getBroadcast(requireActivity().getApplicationContext(), 0,
+                    pinnedWidgetCallbackIntent, PendingIntent.FLAG_IMMUTABLE);
+
+
+            mAppWidgetManager.requestPinAppWidget(myProvider, b, successCallback);
+
+            requireActivity().getApplicationContext().sendBroadcast(pinnedWidgetCallbackIntent);
+
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == stop1Button) {
+            createWidget("Campus elviña");
+        }
+        if (v == stop2Button) {
+            createWidget("Paxariñas");
+        }
+        if (v == stop3Button) {
+            createWidget("Antonio Insua Rivas, 53");
+        }
     }
 }
